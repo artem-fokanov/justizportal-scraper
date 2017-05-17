0fcbd7420f92d923d88b3f52de6fdd69caa0a2d4
zvu <artem.fokanov@gmail.com>
2017-05-17 19:53:48 +0300

    testAddress.php

---
+++ testAddress.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'classes' . DIRECTORY_SEPARATOR . 'SyntaxParser.php';
+require_once 'classes' . DIRECTORY_SEPARATOR . 'Database.php';
+
+$db = new Database();
+$query = $db->query('SELECT * FROM article')->fetchAll($db::FETCH_ASSOC);
+?>
+<table>
+    <thead>
+    <tr>
+        <th>1</th>
+        <th>2</th>
+        <th>3</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($query as $item) :
+        $a = SyntaxParser::parseAddress($item['entity'], $item['plaintext']);?>
+    <tr>
+        <td><?=$item['entity']?></td>
+        <td><?=$a?></td>
+        <td><?=$item['plaintext']?></td>
+    </tr>
+    <?php endforeach; ?>
+
+
+
+    </tbody>
+</table>
+$c = 1;
