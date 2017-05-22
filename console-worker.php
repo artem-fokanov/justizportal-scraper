65a977ad4aa15dbd09f7204231ee500e454ba276
zvu <artem.fokanov@gmail.com>
2017-05-22 22:45:40 +0300

    Console-worker: pass registerant option

--- console-worker.php
+++ console-worker.php
@@ -1,7 +1,20 @@
 <?php
 require_once 'autoload.php';
 
+// default registerant;
 $registerant = 'HRB';
+
+$options = getopt('r:', ['registered:']);
+
+if (!empty($options)) {
+    foreach ($options as $opt => $value) {
+
+        if (in_array($opt, ['r', 'registered']) && in_array(strtoupper($value), ['HRA', 'HRB'])) {
+            $registerant = $value;
+        }
+
+    }
+}
 
 $controller = new \Controller();
 $controller->parser($registerant);
